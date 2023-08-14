11377506dbdac81a25c99388cfa3eff566f2adf8
juanka1293 <juanka1293@gmail.com>
2023-08-13 21:42:15 -0500

    commit 6,captura informacion formulario evento

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const userForm = document.getElementById("user-form");
+    const userList = document.getElementById("user-list");
+    const cancelButton = document.getElementById("cancel-button"); 
+
+    let users = [];
+    let editingUserId = null;
+
+    userForm.addEventListener("submit", function (e) { 
+        e.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const status = document.getElementById("status").value;
+        const name = document.getElementById("name").value;
+
+
+        if (editingUserId) {
+            updateUser(editingUserId, username, status, name);
+        } else {
+            addUser(username, status, name);
+        }
+
+        userForm.reset();
+        editingUserId = null;
+        cancelButton.style.display = "none";
+    });
 });
