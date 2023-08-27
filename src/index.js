0829d1a274c98d69d16899c9346d75c1b4ffa660
juanka1293 <juanka1293@gmail.com>
2023-08-27 16:17:52 -0500

    estructura de proyecto v.2

--- src/index.js
+++ src/index.js
@@ -1,100 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const userForm = document.getElementById("user-form");
-    const userList = document.getElementById("user-list");
-    const cancelButton = document.getElementById("cancel-button"); 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-    let users = [];
-    let editingUserId = null;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-    userForm.addEventListener("submit", function (e) { 
-        e.preventDefault();
-
-        const username = document.getElementById("username").value;
-        const status = document.getElementById("status").value;
-        const name = document.getElementById("name").value;
-
-
-        if (editingUserId) {
-            updateUser(editingUserId, username, status, name);
-        } else {
-            addUser(username, status, name);
-        }
-
-        userForm.reset();
-        editingUserId = null;
-        cancelButton.style.display = "none";
-    });
-    cancelButton.addEventListener("click", function () { 
-        userForm.reset();
-        editingUserId = null;
-        cancelButton.style.display = "none";
-    });
-    function addUser(username, status, name) {
-        const user = {
-            id: new Date().getTime(),
-            name: name,
-            username: username,
-            status: status
-        };
-
-        users.push(user);
-        renderUsers();
-    }
-    function updateUser(id, username, status, name) {
-        const userIndex = users.findIndex(user => user.id === id);
-
-        if (userIndex !== -1) {
-            users[userIndex].username = username;
-            users[userIndex].status = status;
-            users[userIndex].name = name;
-            renderUsers();
-        }
-    }
-    function deleteUser(id) {
-        users = users.filter(user => user.id !== id);
-        renderUsers();
-    }
-    function renderUsers() {
-        userList.innerHTML = "";
-
-        users.forEach(user => {
-            const listItem = document.createElement("li");
-            listItem.className = "list-group-item";
-            listItem.textContent = user.username;
-            listItem.textContent = user.name;
-
-            if (user.status === "activo") {
-                listItem.classList.add("active");
-            } else {
-                listItem.classList.add("inactive");
-            }
-
-            const editButton = document.createElement("button");
-            editButton.className = "ml-2 btn btn-sm btn-primary mr-2";
-            editButton.textContent = "Editar";
-            editButton.addEventListener("click", function () {
-                editUser(user);
-            });
-
-            const deleteButton = document.createElement("button");
-            deleteButton.className = "ml-2 btn btn-sm btn-danger";
-            deleteButton.textContent = "Eliminar";
-            deleteButton.addEventListener("click", function () {
-                deleteUser(user.id);
-            });
-
-            listItem.appendChild(editButton);
-            listItem.appendChild(deleteButton);
-
-            userList.appendChild(listItem);
-        });
-    }
-    function editUser(user) {
-        document.getElementById("user-id").value = user.id;
-        document.getElementById("username").value = user.username;
-        document.getElementById("status").value = user.status;
-        document.getElementById("name").value = user.name;
-        cancelButton.style.display = "inline";
-        editingUserId = user.id;
-    }
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
