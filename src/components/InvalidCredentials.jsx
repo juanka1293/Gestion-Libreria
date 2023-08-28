9fddda594d804a1fe657856816cdbd3a0089a2bb
juanka1293 <juanka1293@gmail.com>
2023-08-27 20:44:55 -0500

    componente invalid creentials

---
+++ src/components/InvalidCredentials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/InvalidCredentials.css'; // Asegúrate de importar tus estilos CSS
+
+const InvalidCredentialsMessage = () => {
+  return (
+    <div className="invalid-credentials-message">
+      <p>Credenciales Inválidas</p>
+    </div>
+  );
+};
+
+export default InvalidCredentialsMessage;
